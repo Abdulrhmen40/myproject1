c728c4a96c0c2a461f40cdb79b6b467a00a39a5e
abdulrhman <abdalrhmen305@gmail.com>
2023-01-11 17:14:16 +0200

    creat img

--- script.js
+++ script.js
@@ -61,3 +61,26 @@ function toggleMore(btn) {
     btn.textContent = "اقرأ المزيد";
   }
 }
+
+ 
+  const modal = document.getElementById("imageModal");
+  const modalImg = document.getElementById("modalImage");
+  const closeBtn = document.querySelector(".close");
+
+  document.querySelectorAll(".gallery-image").forEach(img => {
+    img.addEventListener("click", () => {
+      modal.style.display = "block";
+      modalImg.src = img.src;
+    });
+  });
+
+  closeBtn.onclick = () => modal.style.display = "none";
+
+  modal.onclick = (e) => {
+    if (e.target === modal) {
+      modal.style.display = "none";
+    }
+  };
+
+
+  
