ad0d7bbb9ba36721cc14f364ed9df0486a972595
abdulrhman <abdalrhmen305@gmail.com>
2026-01-04 20:23:18 +0200

    creat myproject1

--- script.js
+++ script.js
@@ -16,3 +16,20 @@ function showVideos() {
   document.querySelectorAll(".media-image").forEach(el => el.style.display = "none");
   document.querySelectorAll(".media-video").forEach(el => el.style.display = "block");
 }
+ document
+        .getElementById("bookingForm")
+        .addEventListener("submit", function (e) {
+          e.preventDefault();
+
+          let name = document.getElementById("name").value;
+          let email = document.getElementById("email").value;
+           let notes = document.getElementById("notes").value;
+
+          let message = `مرحبا عبدالرحمن,%0Aلدي طلب حجز:%0A- الاسم: ${name}%0A- البريد: ${email}%0A- ملاحظات: ${notes}`;
+
+          // الرقم الذي وضعته: 0128467955
+          let whatsappURL = `https://wa.me/249995255549?text=${message}`;
+          window.open(whatsappURL, "_blank");
+
+          this.reset();
+        });
